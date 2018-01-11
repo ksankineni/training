1e5c5bd21b14ee972330d2d21785857b224c0b7c
ksankineni <ksankineni@prokarma.com>
2018-01-11 18:05:48 +0530

    feature2 commit

--- ExampleGitHub/src/FeatureOne.java
+++ ExampleGitHub/src/FeatureOne.java
@@ -2,5 +2,6 @@
 public class FeatureOne {
 	public static void main(String[] args) {
 		System.out.println("This is a branch of master");
+		System.out.println("This is another branch of master");
 	}
 }

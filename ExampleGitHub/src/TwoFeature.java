315d33feb156641649bce7e6582874ebb3eb27f1
ksankineni <ksankineni@prokarma.com>
2018-01-11 19:45:32 +0530

    rename in master

--- ExampleGitHub/src/TwoFeature.java
+++ ExampleGitHub/src/TwoFeature.java
@@ -1,5 +1,5 @@
 
-public class FeatureOne {
+public class TwoFeature {
 	public static void main(String[] args) {
 		System.out.println("This is a branch of master");
 		System.out.println("This is another branch of master");
